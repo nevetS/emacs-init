ff83054d8ecf35f49e599d60a9344f4f6222d28c
Steve Kallestad <nevetS@users.noreply.github.com>
2019-05-27 02:01:15 -0700

    sh renamed to shell

--- configs/org/babel.el
+++ configs/org/babel.el
@@ -2,7 +2,7 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
-   (sh . t)))
+   (shell . t)))
 
 ;; don't require confirmation on C-c C-c for source code block
 (setq org-confirm-babel-evaluate nil)
