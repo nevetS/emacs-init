d619de290cf7ca138d890205565b4d3d0b0fbec0
Steve Kallestad <nevetS@users.noreply.github.com>
2019-05-27 02:03:10 -0700

    mucking with capture templates, WIP

--- configs/org/capture-templates.el
+++ configs/org/capture-templates.el
@@ -12,7 +12,15 @@
                "* TODO Review %c\n%U\n" :immediate-finish t)
               ("m" "Meeting" entry (file "~/Documents/tasks/refile.org")
                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("p" "Phone call" entry (file "~/Documents/tasks/refile.org")
-               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ;; ("p" "Phone call" entry (file "~/Documents/tasks/refile.org")
+              ;;  "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("p"                ;`org-capture' binding + h
+                 "Hugo post"
+                 entry
+                 ;; It is assumed that below file is present in `org-directory'
+                 ;; and that it has a "Blog Ideas" heading. It can even be a
+                 ;; symlink pointing to the actual location of all-posts.org!
+                 (file+olp "all-posts.org" "Blog Ideas")
+                 (function org-hugo-new-subtree-post-capture-template))
               ("h" "Habit" entry (file "~/Documents/tasks/refile.org")
                "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
