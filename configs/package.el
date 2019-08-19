730168ab67ed16a1ca75519bfecda50fcf608de1
Steve Kallestad <skallestad@washlaundry.com>
2019-08-18 20:39:32 -0700

    https repos

--- configs/package.el
+++ configs/package.el
@@ -11,7 +11,8 @@
 
 ;; Note on jedi - there are further installation setps that need to be followed
 ;; on a first run:  https://tkf.github.io/emacs-jedi/latest/
-
+(eval-when-compile (defvar gnutls-algorithm-priority)) ; defined in 'package
+(eval-when-compile (defvar sk:package-list)) ; defined in 'package
 (when (require 'package nil ':noerror)
 
 ;;; any packages that are being installed or tried out belong here
@@ -19,14 +20,15 @@
 
   (require 'package)
   (package-initialize)
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
   ;; elpa archive is installed by default, add melpa, melpa-stable, and gnu
   (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.org/packages/") t)
+	       '("melpa" . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives
-	       '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+	       '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+    (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 
   ;; make sure to have downloaded archive description.
   (or (file-exists-p package-user-dir)
