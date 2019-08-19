9794ad514052f4798a1f66e8673726ec941726e4
Steve Kallestad <skallestad@washlaundry.com>
2019-08-18 22:06:04 -0700

    switch to rg from ag

--- configs/ivy.el
+++ configs/ivy.el
@@ -23,7 +23,7 @@
 
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-c k") 'counsel-rg)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 
